f496240b3284e86460e7109ec9941539a500e5e5
Louis <louisdelarue06@gmail.com>
2024-11-25 09:16:30 +0100

    template html css soon l'équipe on s'attrape tu connais le s et évite de me clc romain j'en ai marr

--- hangman-web.go
+++ hangman-web.go
@@ -1,24 +1,3 @@
-/*
-package main
-
-import (
-
-	"fmt"
-	"net/http"
-
-)
-
-	func main() {
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Welcome to my website!")
-		})
-
-		fs := http.FileServer(http.Dir("static/"))
-		http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-		http.ListenAndServe(":80", nil)
-	}
-*/
 package main
 
 import (
@@ -33,7 +12,6 @@ import (
 	"time"
 )
 
-// Structure du jeu
 type Game struct {
 	Word          string
 	RevealedWord  []rune
@@ -42,21 +20,18 @@ type Game struct {
 	HangmanStages []string
 }
 
-// Variables globales
 var (
 	mots   []string
 	etapes []string
 	mutex  sync.Mutex
-	games  = make(map[string]*Game) // Stockage des sessions de jeu
+	games  = make(map[string]*Game)
 )
 
 func init() {
-	// Charger les mots et les étapes du pendu au démarrage
 	mots = chargerMots("words.txt")
 	etapes = chargerPendu("hangman.txt")
 }
 
-// Charger la liste des mots
 func chargerMots(fichier string) []string {
 	file, err := os.Open(fichier)
 	if err != nil {
@@ -79,7 +54,6 @@ func chargerMots(fichier string) []string {
 	return mots
 }
 
-// Charger les étapes du pendu
 func chargerPendu(fichier string) []string {
 	file, err := os.Open(fichier)
 	if err != nil {
@@ -112,13 +86,11 @@ func chargerPendu(fichier string) []string {
 	return stages
 }
 
-// Choisir un mot aléatoire
 func choisirMot(mots []string) string {
 	rand.Seed(time.Now().UnixNano())
 	return mots[rand.Intn(len(mots))]
 }
 
-// Créer une nouvelle partie
 func nouvellePartie() *Game {
 	word := choisirMot(mots)
 	return &Game{
@@ -130,7 +102,6 @@ func nouvellePartie() *Game {
 	}
 }
 
-// Révéler quelques lettres du mot
 func revelerLettres(word string, n int) []rune {
 	revealed := make([]rune, len(word))
 	for i := range revealed {
@@ -143,7 +114,6 @@ func revelerLettres(word string, n int) []rune {
 	return revealed
 }
 
-// Gérer la session de jeu
 func getGame(r *http.Request) *Game {
 	mutex.Lock()
 	defer mutex.Unlock()
@@ -156,13 +126,11 @@ func getGame(r *http.Request) *Game {
 		return game
 	}
 
-	// Créer une nouvelle session
 	newGame := nouvellePartie()
 	games[sessionID] = newGame
 	return newGame
 }
 
-// Sauvegarder la session
 func saveGame(w http.ResponseWriter, r *http.Request, game *Game) {
 	mutex.Lock()
 	defer mutex.Unlock()
@@ -175,13 +143,11 @@ func saveGame(w http.ResponseWriter, r *http.Request, game *Game) {
 	games[sessionID] = game
 }
 
-// Gérer les requêtes
 func gameHandler(w http.ResponseWriter, r *http.Request) {
 	game := getGame(r)
 
-	// Gérer une soumission de lettre
 	if r.Method == http.MethodPost {
-		letter := r.FormValue("lettre") // Utiliser 'lettre' comme clé pour la lettre
+		letter := r.FormValue("lettre")
 		if len(letter) == 1 {
 			letterRune := rune(letter[0])
 			if !game.UsedLetters[letterRune] {
@@ -199,18 +165,15 @@ func gameHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	// Déterminer l'état du jeu (victoire ou défaite)
 	win := strings.Compare(game.Word, string(game.RevealedWord)) == 0
 	lose := game.AttemptsLeft <= 0
 
-	// Charger le template HTML
 	tmpl, err := template.ParseFiles("hangman.tmpl")
 	if err != nil {
 		http.Error(w, "Erreur lors du chargement du template HTML", http.StatusInternalServerError)
 		return
 	}
 
-	// Rendu de la page avec les données du jeu
 	data := map[string]interface{}{
 		"EtatMot":             afficherMotRevele(game.RevealedWord),
 		"TentativesRestantes": game.AttemptsLeft,
@@ -221,20 +184,18 @@ func gameHandler(w http.ResponseWriter, r *http.Request) {
 		"HangmanStage":        game.HangmanStages[10-game.AttemptsLeft],
 	}
 
-	saveGame(w, r, game) // Sauvegarder l'état du jeu
+	saveGame(w, r, game)
 	tmpl.Execute(w, data)
 }
 
-// Afficher le mot révélé avec des espaces
 func afficherMotRevele(revealedWord []rune) string {
 	return strings.Join(strings.Split(string(revealedWord), ""), " ")
 }
 
 func main() {
-	// Gérer les fichiers statiques (CSS, images, etc.)
+
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	// Route pour le jeu
 	http.HandleFunc("/", gameHandler)
 
 	// Démarrer le serveur
